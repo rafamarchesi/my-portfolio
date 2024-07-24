8e2e5c51d95fa837d3394acbd1b05c67686970a8
Rafaela <rafamarchesi2@gmail.com>
2024-07-24 15:02:39 -0300

    refactor: restructure navbar component for better readability

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,12 +1,122 @@
-
+import React, { useState } from 'react';
+import { AppBar, Toolbar, IconButton, Drawer, List, ListItem, ListItemIcon, useTheme, Box, useMediaQuery } from '@mui/material';
+import MenuIcon from '@mui/icons-material/Menu';
+import Face3Icon from '@mui/icons-material/Face3';
+import DesignServicesIcon from '@mui/icons-material/DesignServices';
+import CodeIcon from '@mui/icons-material/Code';
+import PhonelinkIcon from '@mui/icons-material/Phonelink';
+import ContactMailIcon from '@mui/icons-material/ContactMail';
 
 const NavBar = () => {
+  const [drawerOpen, setDrawerOpen] = useState(false);
+  const theme = useTheme();
+  const isMobile = useMediaQuery(theme.breakpoints.down('sm'));
+
+  const handleDrawerToggle = () => {
+    setDrawerOpen(!drawerOpen);
+  };
+
+  const listItemIconStyles = {
+    color: theme.palette.primary.main,
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: '20px',
+    position: 'relative',
+    cursor: 'pointer',
+    '&:hover::before': {
+      content: 'attr(data-tooltip)',
+      position: 'absolute',
+      left: 'calc(-100% - 10px)',
+      backgroundColor: theme.palette.primary.main,
+      color: theme.palette.common.white,
+      padding: '5px 10px',
+      borderRadius: '4px',
+      whiteSpace: 'nowrap',
+      zIndex: 1,
+    },
+  };
+
+  const toolbarStyles = {
+    justifyContent: 'flex-end',
+    display: { xs: 'flex', sm: 'none' }
+  };
+
+  const boxStyles = {
+    width: 100,
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    paddingTop: theme.spacing(2),
+  };
+
+  const drawerStyles = {
+    '& .MuiDrawer-paper': {
+      border: 'none',
+      width: 200,
+      justifyContent: 'center',
+      alignItems: 'end',
+      position: 'absolute', // ensures the drawer does not scroll with content
+    },
+  };
 
   return (
     <>
-      navbar
+      <AppBar style={{ backgroundColor: theme.palette.common.white, position: 'static' }}>
+        <Toolbar sx={toolbarStyles}>
+          {isMobile && (
+            <IconButton
+              edge="end"
+              color="inherit"
+              aria-label="menu"
+              onClick={handleDrawerToggle}
+              sx={{ color: theme.palette.primary.main }}
+            >
+              <MenuIcon fontSize="large" />
+            </IconButton>
+          )}
+        </Toolbar>
+      </AppBar>
+      <Drawer
+        anchor="right"
+        open={isMobile ? drawerOpen : true}
+        onClose={handleDrawerToggle}
+        variant={isMobile ? 'temporary' : 'persistent'}
+        sx={drawerStyles}
+        PaperProps={{ sx: drawerStyles }}
+      >
+        <Box sx={boxStyles}>
+          <List>
+            <ListItem>
+              <ListItemIcon sx={listItemIconStyles} data-tooltip="About">
+                <Face3Icon fontSize="medium" />
+              </ListItemIcon>
+            </ListItem>
+            <ListItem>
+              <ListItemIcon sx={listItemIconStyles} data-tooltip="Skills">
+                <DesignServicesIcon fontSize="medium" />
+              </ListItemIcon>
+            </ListItem>
+            <ListItem>
+              <ListItemIcon sx={listItemIconStyles} data-tooltip="Projects">
+                <CodeIcon fontSize="medium" />
+              </ListItemIcon>
+            </ListItem>
+            <ListItem>
+              <ListItemIcon sx={listItemIconStyles} data-tooltip="Contact">
+                <PhonelinkIcon fontSize="medium" />
+              </ListItemIcon>
+            </ListItem>
+            <ListItem>
+              <ListItemIcon sx={listItemIconStyles} data-tooltip="Email">
+                <ContactMailIcon fontSize="medium" />
+              </ListItemIcon>
+            </ListItem>
+          </List>
+        </Box>
+      </Drawer>
     </>
-  )
-}
+  );
+};
 
-export default NavBar
+export default NavBar;
